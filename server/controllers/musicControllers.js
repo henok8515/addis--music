4398c6fd01e998af393f6034d02fb01320329e6f
henok8515 <henokegezew44@gmail.com>
2023-03-04 17:52:21 +0300

    implementing the edit and delete functionality

--- server/controllers/musicControllers.js
+++ server/controllers/musicControllers.js
@@ -4,9 +4,9 @@ module.exports.getMusics = async (req, res) => {
   const musics = await Music.find();
   res.json(musics);
 };
-
 module.exports.createMusic = async (req, res) => {
   const music = new Music({
+    title: req.body.title,
     artist: req.body.artist,
     genre: req.body.genre,
   });
@@ -14,21 +14,21 @@ module.exports.createMusic = async (req, res) => {
     await music.save();
     res.json(music);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error.message);
   }
 };
-
 module.exports.updateMusic = async (req, res) => {
-  const { _id, artist, genre } = req.body;
-  Music.findByIdAndUpdate(_id, { artist, genre })
+  const { id, title, artist, genre } = req.body;
+  Music.findByIdAndUpdate(id, { title, artist, genre })
     .then(() => res.json("updated successfully"))
     .catch((err) => {
       res.json(err);
     });
 };
 module.exports.deleteMusic = async (req, res) => {
-  const { _id } = req.body;
-  Music.findByIdAndDelete(_id)
+  const { id } = req.body;
+  console.log(id);
+  Music.findByIdAndDelete(id)
     .then(() => res.json("Deleted successfully"))
     .catch((err) => {
       res.json(err);
